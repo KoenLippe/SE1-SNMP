4d5566de8d7c7ab2c0f5de60a3007928c0429bff
Koen Lippe <koen.lippe@quicknet.nl>
2019-02-10 16:18:36 +0100

    Processed second review

--- w1/w1o1/w1o1.py
+++ w1/w1o1/w1o1.py
@@ -8,16 +8,16 @@ __name__ = 'w1o1.py'
 # Print "hello world" uitbreiding
 def combine(arg1, arg2):
     """ Concatenates arg1 and arg2 with an space to seperate the args """
-    string = arg1 + " " + arg2
-    return string
+    return arg1.strip() + " " + arg2.strip() # Updated after recieving feedback by James Watson (2nd time)
+
 
 
 # Controleer op het programma goed werkt.
 # Maak zelf nog meer tests.
 if __name__ == 'w1o1.py':
-    print(combine("hello", "world"))
+    print(combine("  hello", "world  "))
     print(combine("Koen", "Lippe"))
-    print(combine("test", "123"))
+    print(combine("test", "  123"))
 
-    if combine("hello", "world") != "hello world":
+    if combine("  hello", "world  ") != "hello world":
         print("Error\n")
